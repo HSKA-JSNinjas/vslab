6fe6b572b3ed30f1fb5980f2f76186b020ce4cc1
Dominik Finkbeiner <mail@dfinkbeiner.de>
2016-06-14 21:14:22 +0200

    add turbine

--- turbine-server/src/main/java/de/hska/vslab/TurbineServerApplication.java
+++ turbine-server/src/main/java/de/hska/vslab/TurbineServerApplication.java
@@ -2,10 +2,14 @@ package de.hska.vslab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableDiscoveryClient
+@EnableHystrix
 public class TurbineServerApplication {
 
 	public static void main(String[] args) {
