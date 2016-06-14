bf6b9425d120de9af245c1ea756e90b0d48b8871
Maurice Breit <maurice.breit@sap.com>
2016-06-14 22:20:10 +0200

    fix turbine

--- turbine-server/src/main/java/de/hska/vslab/TurbineServerApplication.java
+++ turbine-server/src/main/java/de/hska/vslab/TurbineServerApplication.java
@@ -1,6 +1,7 @@
 package de.hska.vslab;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -9,7 +10,6 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @SpringBootApplication
 @EnableTurbine
 @EnableDiscoveryClient
-@EnableHystrix
 public class TurbineServerApplication {
 
 	public static void main(String[] args) {
